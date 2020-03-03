dd77c66571b2d6ddcc5e8d591fa25b84b0fb0167
charlyhackr <930@holbertonschool.com>
2020-03-02 22:19:22 -0500

    add new code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""
+Shows the complete tasks done by a specific employee ID
+"""
+
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Get the todo list
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    t = requests.get(url)
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    # Get the user info
+    url = 'https://jsonplaceholder.typicode.com/users'
+    u = requests.get(url)
+
+    # Convert to json
+    todos = t.json()
+    users = u.json()
+
+    user_dict = {}
+
+    for user in users:
+        username = user['username']
+        user_infos = []
+
+        for task in todos:
+            if task['userId'] == user['id']:
+                dict_info = {}
+                dict_info['username'] = username
+                dict_info['task'] = task['title']
+                dict_info['completed'] = task['completed']
+                user_infos.append(dict_info)
+        user_dict[user['id']] = user_infos
+
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(user_dict, f)
